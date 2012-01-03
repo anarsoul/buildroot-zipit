ecc0e567948c02d91fc6b88ab37411048c406c76
Vasily Khoruzhick <anarsoul@gmail.com>
2012-01-03 23:13:25 +0300

    xterm: fix compilation with ncursesw
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -10,4 +10,12 @@ XTERM_SITE = ftp://invisible-island.net/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw
 XTERM_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
+ifeq ($(BR2_USE_WCHAR),y)
+XTERM_CONF_OPT = --enable-wide-chars
+define XTERM_NCURSESW_FIX
+	$(SED) 's/ncurses/ncursesw/' $(@D)/configure
+endef
+XTERM_PRE_CONFIGURE_HOOKS += XTERM_NCURSESW_FIX
+endif
+
 $(eval $(call AUTOTARGETS))
